eeb2a9099b1a632fe658325600d4aa2df418d20d
joshbuddy <joshbuddy@gmail.com>
2019-08-06 17:11:33 -0700

    Switch to yarn, new webpack

--- scripts/build.js
+++ scripts/build.js
@@ -3,8 +3,8 @@ require('babel-register');
 const webpack = require('webpack');
 const config = require('../webpack.config.js').server;
 const bundler = webpack(config);
-const config2 = require('../webpack.config.js').client;
-const bundler2 = webpack(config2);
+// const config2 = require('../webpack.config.js').client;
+// const bundler2 = webpack(config2);
 
 bundler.run((err, stats) => {
   if (err) {
@@ -13,9 +13,9 @@ bundler.run((err, stats) => {
   console.log(stats.toString());
 });
 
-bundler2.run((err, stats) => {
-  if (err) {
-    console.error(err);
-  }
-  console.log(stats.toString());
-});
+// bundler2.run((err, stats) => {
+//   if (err) {
+//     console.error(err);
+//   }
+//   console.log(stats.toString());
+// });
--- webpack.config.js
+++ webpack.config.js
@@ -1,151 +1,9 @@
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+const path = require('path')
 
-const development = {
+module.exports = {
+  mode: 'development',
   output: {
-    path: require('path').join(__dirname + '/dist'),
-    filename: '[name].js',
-    publicPath: '/'
-  },
-  debug: true,
-  devtool: 'eval',
-  entry: {
-    polyfill: 'eventsource-polyfill',
-    hot: 'webpack-hot-middleware/client',
-    server: './src/index',
-    client: './src/client',
-  },
-  stats: {
-    colors: true,
-    reasons: true
-  },
-  resolve: {
-    extensions: ['', '.js'],
-  },
-  module: {
-    preLoaders: [{
-      test: /\.js$/,
-      exclude: [/node_module/, 'mock/*'],
-      loader: 'eslint'
-    }],
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel',
-      options: {
-        presets: ["env", {
-          "targets": { "chrome": 62 }
-        }]
-      }
-    }, {
-      test: /\.scss/,
-      exclude: [/node_module/],
-      loader: 'style!css?localIdentName=[path][name]---[local]---[hash:base64:5]!postcss'
-    }, {
-      test: /\.css/,
-      exclude: [/node_module/],
-      loader: 'style!css'
-    }, {
-      test: /\.(png|jpg|woff|woff2)$/,
-      loader: 'url?limit=8192'
-    }]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new webpack.DefinePlugin({
-      __DEVELOPMENT__: true
-    }),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html.tpl',
-      inject: 'body',
-      chunks: ['polyfill', 'hot', 'server'],
-      filename: 'server.html',
-    }),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html.tpl',
-      inject: 'body',
-      chunks: ['polyfill', 'hot', 'client'],
-      filename: 'client.html',
-    }),
-  ],
-  postcss() {
-    return [
-      require("postcss-import")({addDependencyTo: webpack}),
-      require("postcss-url")(),
-      require("postcss-cssnext")(),
-      require('precss'),
-      require("postcss-browser-reporter")(),
-      require("postcss-reporter")()
-    ];
-  }
-};
-
-const client = {
-  output: {
-    path: require('path').join(__dirname + '/dist'),
-    filename: 'client.js',
-    publicPath: '/'
-  },
-  devtool: 'sourcemap',
-  entry: './src/game/client.js',
-  stats: {
-    colors: true,
-    reasons: true
-  },
-  resolve: {
-    extensions: ['', '.js'],
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel'
-    }, {
-      test: /\.scss/,
-      exclude: [/node_module/],
-      loader: 'style!css!postcss'
-    }, {
-      test: /\.css/,
-      exclude: [/node_module/],
-      loader: 'style!css'
-    }, {
-      test: /\.(png|jpg|woff|woff2)$/,
-      loader: 'url?limit=8192'
-    }]
-  },
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.NoErrorsPlugin(),
-    new webpack.DefinePlugin({
-      __DEVELOPMENT__: false
-    }),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html.tpl',
-      inject: 'body'
-    })
-  ],
-  postcss() {
-    return [
-      require("postcss-import")({addDependencyTo: webpack}),
-      require("postcss-url")(),
-      require("postcss-cssnext")(),
-      require('precss'),
-      require("postcss-browser-reporter")(),
-      require("postcss-reporter")()
-    ];
-  }
-};
-
-const server = {
-  output: {
-    path: require('path').join(__dirname + '/dist'),
+    path: path.join(__dirname + '/dist'),
     filename: 'server.js',
     publicPath: '/',
     library: 'TicTacToe',
@@ -157,24 +15,22 @@ const server = {
     colors: true,
     reasons: true
   },
-  resolve: {
-    extensions: ['', '.js'],
-  },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel'
-    }]
-  },
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new webpack.DefinePlugin({
-      __DEVELOPMENT__: false
-    })
-  ],
+    rules: [
+      {
+        test: /\.node$/,
+        use: 'node-loader'
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
-
-module.exports = { development, client, server };
